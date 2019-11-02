54ce346de51e5115e3e83c649ea1677aa9f63a71
birchwave <birchwave@gmail.com>
2019-11-02 15:34:51 -0400

    make a unique oneline comment for my_branch

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,5 @@
 ### prints square of 3 = 9 :)
-this is a bit more docuumentation
-adding one more comment line in branch my_branch
-and, last but not least adding one more line
+this is the comment in branch my_branch
 ###
 def my_square(y):
   return(y ** 2)
