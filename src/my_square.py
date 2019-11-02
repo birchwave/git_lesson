fbc68bd5b2a17d6e10643bff124c2619fc229f40
birchwave <birchwave@gmail.com>
2019-11-02 14:38:40 -0400

    and adding one more line to comments

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,7 @@
 ### prints square of 3 = 9 :)
 this is a bit more docuumentation
 adding one more comment line in branch my_branch
+and, last but not least adding one more line
 ###
 def my_square(y):
   return(y ** 2)
