bc284828a9dba70063cd867693ca86042b4bd7b8
birchwave <birchwave@gmail.com>
2019-11-02 10:09:17 -0400

    square(5) and x -> y

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
 ### prints square of 3 = 9 :)
 this is a bit more docuumentation
 ###
-def my_square(x):
-  return(x ** 2)
+def my_square(y):
+  return(y ** 2)
 
-print(my_square(4))
+print(my_square(5))
